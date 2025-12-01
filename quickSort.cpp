4749111c62e0023151ec6a146a32fbfbf3646449
Arnav Singla <arnavsingla06@gmail.com>
2025-12-01 22:23:02 +0530

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,47 +1,50 @@
-/*In quick sort , we choose any element to be pivot
-we  traverse through the whole array and assign the pivot its correct position 
-then the array is divided into two halves (left of pivot and right of pivot)
-we recursively perform same operation for both halves by selecting new pivots*/
+#include<bits/stdc++.h>
 
-#include <iostream>
-#include<bits/stdc++.h> 
 using namespace std;
 
-int partition(vector<int> &arr,int st,int end ){
-        int idx=st;
-        int pvt=arr[end];
-        for(int j=st;j<end;j++){
-            
-            if(arr[j]<=pvt){
-              
-                swap(arr[j],arr[idx]);    
-               idx++;
-            }
-        }
-        //now the position of pivot will be
-        //one more than current idx
-        
-        swap(arr[idx],arr[end]);
-        return idx;
-
-}
-void quickSort(vector<int> &arr, int st, int end){
-    if(st<end){
-       int pivIdx =  partition(arr , st ,end);
-       quickSort(arr,st,pivIdx-1);//left half
-       quickSort(arr,pivIdx+1,end);//right half
-
+int pivotIndex(int low, int high,vector<int>& arr){
+  
+  int pivot = arr[low];
+  
+  int i = low;
+  int j = high-1;
+  while(i<j){
+    do{
+      i++;
+    }while(arr[i]<=pivot);
+    
+    do{
+      j--;
+    }while(arr[j]>pivot);
+    
+    if(i<j){
+      swap(arr[i],arr[j]);
     }
-}
-int main() {
-    vector<int> arr={23,45,32,12,13,7};
-    quickSort(arr,0,arr.size()-1);
-
-    for(int x:arr){
-        cout<<x<<" ";
-    }
+    
+  }
+  swap(arr[low],arr[j]);
+  return j;
 }
 
-// TC - O(nlogn)
-// worst case when pivot is largest or smalles O(n^2)
-// SC- O(1)
+vector<int> quickSort(int low,int high,vector<int>& arr){
+  
+  if(low<high){
+    int j = pivotIndex(low,high,arr);
+    quickSort(low,j,arr);
+    quickSort(j+1,high,arr);
+  }
+return arr;
+  
+}
+
+int main() 
+{
+  vector<int> arr = {10,5,8,12,15,6,3,9,16};
+  int l = 0;
+  int h = arr.size();
+  
+  vector<int> ans = quickSort(l,h,arr);
+  for(auto i:ans){
+    cout<<i<<" ";
+  }
+}
